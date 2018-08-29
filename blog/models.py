ee824a00a810c96d059dcf21da6d3a9d766e9798
oneGene <jinpark.han@gmail.com>
2018-08-29 06:42:59 +0100

    Finished chapter 3

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django import forms
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.snippets.models import register_snippet
 
 
 class BlogPage(Page):
@@ -15,7 +22,33 @@ class BlogPage(Page):
 
 class PostPage(Page):
     body = RichTextField(blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+    tags = ClusterTaggableManager(through='blog.BlogPageTag', blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('body', classname="full"),
+        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        FieldPanel('tags'),
     ]
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=80)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('slug')
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('PostPage', related_name='post_tags', on_delete=models.CASCADE)
